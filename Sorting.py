9bc5b19888c23de329499248aa9a8c24c2877a80
teohangxanh <47458301+teohangxanh@users.noreply.github.com>
2020-02-21 09:07:45 -0600

    .

---
+++ Sorting.py
@@ -0,0 +1,182 @@
+'''
+Author: Ted Dang
+Description: 
+    This programs is to sort a list of integers
+    The only parameter is the list of integers to be sorted
+'''
+
+import math
+
+#This function returns a list of digits of n from right to left
+def getDigits(n):
+    ans = []
+    while n > 0:
+        rem = n % 10
+        ans.append(rem)
+        n = int(n / 10)
+    return ans
+
+#Swap two numbers in a list using their indices
+def swapPos(a, i, j):
+    temp = a[i]
+    a[i] = a[j]
+    a[j] = temp
+ 
+#Swap two numbers' remues
+def swaprem(a, b):
+    temp = a
+    a = b
+    b = temp
+    
+def shift_right(alist, left, right):
+    if (left > right or len(alist) < left or len(alist) < right):
+        return -1
+    temp = alist[right]
+    for i in range(right, left, -1):
+        alist[i] = alist[i - 1]
+    alist[left] = temp    
+    
+def a_bubble(alist):
+    if len(alist) < 2:
+        return alist
+    for i in range(len(alist) - 1):
+        #See if the list was already sorted
+        swapped = False
+        for j in range(len(alist) - 1 - i):
+            if (alist[j] > alist[j + 1]):
+                swapPos(alist, j, j + 1)
+                swapped = True
+        #As there is no swap, the list was already sorted      
+        if (swapped == False):
+            break
+    return alist           
+ 
+def d_bubble(alist):
+    if len(alist) < 2:
+        return alist
+    for i in range(len(alist) - 1):
+        #See if the list was already sorted
+        swapped = False
+        for j in range(len(alist) - 1 - i):
+            if (alist[j] < alist[j + 1]):
+                swapPos(alist, j, j + 1)
+                swapped = True
+        #As there is no swap, the list was already sorted      
+        if (swapped == False):
+            break
+    return alist 
+
+def a_selection(alist):
+    if len(alist) < 2:
+        return alist
+    for i in range(len(alist)):
+        min = alist[i]
+        min_index = i
+        for j in range(i, len(alist)):
+            if alist[j] < min:
+                min = alist[j]
+                min_index = j
+        if (alist[i] != min):
+            swapPos(alist, min_index, i)
+    return alist
+
+def d_selection(alist):
+    if len(alist) < 2:
+        return alist
+    for i in range(len(alist)):
+        max = alist[i]
+        max_index = i
+        for j in range(i, len(alist)):
+            if alist[j] > max:
+                max = alist[j]
+                max_index = j
+        if (alist[i] != max):
+            swapPos(alist, max_index, i)
+    return alist
+        
+def a_insertion(alist):
+    if len(alist) < 2:
+        return alist
+    #Consider from 1 to len
+    for i in range(1, len(alist)):
+        for j in range(i):
+            if alist[i] < alist[j]:
+                shift_right(alist, j, i)
+                break
+    return alist
+
+def d_insertion(alist):
+    if len(alist) < 2:
+        return alist
+    #Consider from 1 to len
+    for i in range(1, len(alist)):
+        for j in range(i):
+            if alist[i] > alist[j]:
+                shift_right(alist, j, i)
+                break
+    return alist
+
+def a_shell(alist):
+    if len(alist) < 2:
+        return alist
+    gap = int(len(alist) / 2)
+    while (gap > 0):
+        for i in range(len(alist) - gap):
+            if alist[i] > alist[i + gap]:
+                swapPos(alist, i, i + gap)
+                #Check and back swap
+                j = i
+                while j - gap >= 0:
+                    if alist[j] < alist[j - gap]:
+                        swapPos(alist, j, j - gap)
+                    j -= gap
+        gap = int(gap / 2)
+    return alist
+
+def d_shell(alist):
+    if len(alist) < 2:
+        return alist
+    gap = int(len(alist) / 2)
+    while (gap > 0):
+        for i in range(len(alist) - gap):
+            if alist[i] < alist[i + gap]:
+                swapPos(alist, i, i + gap)
+                #Check and back swap
+                j = i
+                while j - gap >= 0:
+                    if alist[j] > alist[j - gap]:
+                        swapPos(alist, j, j - gap)
+                    j -= gap
+        gap = int(gap / 2)
+    return alist
+
+def a_radix(alist):
+    digit = 0
+    mx = 0
+    
+    #Find how many digits the max number has
+    for i in range(len(alist)):
+        mx = max(mx, alist[i])
+        
+    #If len(alist) is a power of 10 evenly
+    if math.log(mx, 10) == math.floor(math.log(mx, 10)):
+        digit = math.log(mx, 10)
+    else:
+        digit = math.floor(math.log(mx, 10)) + 1
+    
+    #i represents the number of digits of max
+    tem_list = {}
+    # Key = index, value = value  
+    for i in range(len(alist)):
+        tem_list[i] = alist[i]
+        
+    return 0
+    
+
+            
+        
+    
+    
+    
+    
+        
---
+++ interview.py
@@ -0,0 +1,88 @@
+'''
+Author: Ted Dang
+Description: 
+    This programs is to answer interview questions
+'''
+
+import math
+from numpy import random
+
+# This calculate factorial of n
+def fact(n):
+    ans = 1
+    for i in range(2, n + 1):
+        ans *= i
+    return ans
+
+def isPalindrome(n):
+    my_list = []
+    n = math.floor(n)
+    while n > 0:
+        rem = n % 10
+        my_list.append(rem)
+        n = int(n / 10)
+    for i in range(len(my_list)):
+        if my_list[i] is not my_list[len(my_list) - i - 1]:
+            return False
+    return True
+        
+def findMin(alist):
+    mn = alist[0]
+    for i in range(len(alist)):
+        if mn > alist[i]:
+            mn = alist[i]
+    return mn
+
+        
+def findMax(alist):
+    mx = alist[0]
+    for i in range(len(alist)):
+        if mx < alist[i]:
+            mx = alist[i]
+    return mx
+
+'''Given 2 sorted arrays of integers, code to find a number from each array 
+such that their sum is closest to some integer K'''
+def question3(alist, blist, k):
+    my_list = [[] for _ in range(len(alist))]
+    for i in range(len(alist)):
+        numa = alist[i]
+        for j in range(len(blist)):
+            numb = blist[j]
+            my_list[i].append(abs(numa + numb - k))
+    mn = my_list[0][0]
+    for lst in my_list:
+        for number in lst:
+            if mn > number:
+                mn = number
+    for i in range(len(my_list)):
+        for j in range(len(my_list[i])):
+            if my_list[i][j] == mn:
+                a = alist[i]
+                b = blist[j]
+    return a, b
+
+def displayFib(n):
+    ans = [0] * n
+    for i in range(n):
+        if i < 2:
+            ans[i] = 1
+        elif i>= 2:
+            ans[i] = ans[i - 1] + ans[i - 2]
+    return ans
+
+# This returns permutation of n choose k in an equal probability
+def permu(n, k):
+    return int(fact(n) / fact(n - k))
+
+# This returns a list of n uniformly random numbers from a to b
+def uniform(n, a, b):
+    result = []
+    for i in range(n):
+        number = random.uniform(a, b)
+        result.append(number)
+    return result
+    
+    
+    
+    
